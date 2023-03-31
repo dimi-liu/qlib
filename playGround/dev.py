b617d0973a1c7e9ac7dd948107e5d93b995b8914
dimi <598465018@qq.com>
2023-03-31 18:27:47 +0800

    230331

--- playGround/dev.py
+++ playGround/dev.py
@@ -1,15 +1,29 @@
-import qlib
-from pathlib import Path
-# region in [REG_CN, REG_US]
-from qlib.constant import REG_CN
-
+import hashlib
 
 if __name__ == '__main__':
-    print(qlib.__version__)
-    # 下载数据 路径在 ~/.qlib，然后初始化数据
-    provider_uri = "~/.qlib/qlib_data/cn_data"  # target_dir
-    #
-    # # Initialize Qlib before calling other APIs: run following code in python.
-    qlib.init(provider_uri=provider_uri, region=REG_CN)
+    with open('key') as f: file = f.read()
+    l = file.split()
+    cache = ''
+    for i in l:
+        print(i)
+        print(hashlib.md5(i.encode(encoding='utf-8')).hexdigest())
+        print('')
+        cache += i
+    print(cache)
+    print(hashlib.md5(cache.encode(encoding='utf-8')).hexdigest())
 
-    path_cur = Path.cwd()
+# import qlib
+# from pathlib import Path
+# # region in [REG_CN, REG_US]
+# from qlib.constant import REG_CN
+#
+#
+# if __name__ == '__main__':
+#     print(qlib.__version__)
+#     # 下载数据 路径在 ~/.qlib，然后初始化数据
+#     provider_uri = "~/.qlib/qlib_data/cn_data"  # target_dir
+#     #
+#     # # Initialize Qlib before calling other APIs: run following code in python.
+#     qlib.init(provider_uri=provider_uri, region=REG_CN)
+#
+#     path_cur = Path.cwd()
